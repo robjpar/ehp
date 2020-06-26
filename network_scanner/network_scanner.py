76dcd915ab625973052fab8a14681372d4049545
Robert Paruch <roobbb@live.com>
2020-06-26 03:19:10 -0700

    Update network_scanner.py

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,14 +5,7 @@ def scan(ip):
     arp_request = sc.ARP(pdst=ip)
     broadcast = sc.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
-
-    print(arp_request.summary())
-    print(broadcast.summary())
-    print(arp_request_broadcast.summary())
-
-    arp_request.show()
-    broadcast.show()
-    arp_request_broadcast.show()
-
+    answered, unanswered = sc.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan('10.0.2.1/24')
