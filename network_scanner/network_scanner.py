1405574f9220f6e6b27fa3b85d8829646aa612c1
Robert Paruch <roobbb@live.com>
2020-06-27 01:02:01 -0700

    Update network_scanner.py

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,12 +1,12 @@
 import scapy.all as sc
-import optparse
+import argparse
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target",
-                      help="Target IP or IP range.")
-    options, arguments = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target",
+                        help="Target IP or IP range.")
+    options = parser.parse_args()
     if not options.target:
         parser.error(
             '[-] Please specify a target IP or IP range, use --help for more \
