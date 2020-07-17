ff7d79b42acde89dad95a394ecea81e004b221ed
Robert Paruch <roobbb@live.com>
2020-07-17 03:34:12 -0700

    Update build_download_and_execute.sh

--- download_and_execute/build_download_and_execute.sh
+++ download_and_execute/build_download_and_execute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # pip3 uninstall requests
 # pip3 install requests==2.5.1
-# chmod 755 build_build_download_and_execute.sh
+# chmod 755 build_download_and_execute.sh
 # ./build_download_and_execute.sh
 wine pyinstaller --onefile --noconsole download_and_execute.py
