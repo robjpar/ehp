a40f085d4a1d43c036da4db779090bf29091758e
Robert Paruch <roobbb@live.com>
2020-07-19 15:35:00 -0700

    Update extract_forms.py

--- 19-extract_froms/extract_forms.py
+++ 19-extract_froms/extract_forms.py
@@ -1,17 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-def request(url, timeout=3):
-    try:
-        return requests.get(url, timeout=timeout)
-    except requests.exceptions.ConnectionError:
-        pass
-
+from urllib.parse import urljoin
 
 target_url = 'http://10.0.2.14/mutillidae/index.php?page=dns-lookup.php'
-content = request(target_url).content.decode()
+response = requests.get(target_url)
+content = response.content.decode()
 
 parsed_html = BeautifulSoup(content, features='lxml')
 forms_list = parsed_html.findAll('form')
-print(forms_list)
+
+for form in forms_list:
+    action = form.get('action')
+    post_url = urljoin(target_url, action)
+
+    method = form.get('method')
+
+    inputs_list = form.findAll('input')
+
+    post_data = {}
+    for input_ in inputs_list:
+        input_name = input_.get('name')
+        input_type = input_.get('type')
+        input_value = input_.get('value')
+        if input_type == 'text':
+            input_value = 'test'
+        post_data[input_name] = input_value
+
+    response = requests.post(post_url, data=post_data)
+    content = response.content.decode()
+    print(content)
