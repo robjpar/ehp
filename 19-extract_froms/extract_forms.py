54911e78ee57f9c7ab95472afd676d3bbcb5e09e
Robert Paruch <roobbb@live.com>
2020-07-20 00:30:47 -0700

    Update extract_forms.py

--- 19-extract_froms/extract_forms.py
+++ 19-extract_froms/extract_forms.py
@@ -12,11 +12,9 @@ forms_list = parsed_html.findAll('form')
 for form in forms_list:
     action = form.get('action')
     post_url = urljoin(target_url, action)
-
     method = form.get('method')
 
     inputs_list = form.findAll('input')
-
     post_data = {}
     for input_ in inputs_list:
         input_name = input_.get('name')
